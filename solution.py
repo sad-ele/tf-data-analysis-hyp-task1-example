73249adf24e6c5b8ee7b7d9da2a375eab720ba20
sad-ele <72252304+sad-ele@users.noreply.github.com>
2023-04-13 14:03:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.proportion import proportions_ztest
+from scipy.stats import norm
 
 
 chat_id = 170380561 # Ваш chat ID, не меняйте название переменной
@@ -9,8 +9,21 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    _, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative="larger")
-    return p_value < 0.04 # Ваш ответ, True или False
+    alpha=0.04
+    control_conversion = x_success / x_cnt
+    test_conversion = y_success / y_cnt
+    
+    # Вычисляем комбинированную пропорцию
+    combined_conversion = (x_success + y_success) / (x_cnt + y_cnt)
+    
+    # Вычисляем стандартную ошибку
+    se = np.sqrt(combined_conversion * (1 - combined_conversion) * (1/x_cnt + 1/y_cnt))
+    
+    # Вычисляем Z-статистику
+    z = (control_conversion - test_conversion) / se
+    
+    # Находим критическое значение Z-статистики
+    critical_value = norm.ppf(1 - alpha/2) # двусторонний Z-тест
+    
+    # Принимаем решение на основе Z-статистики
+    return abs(z) > critical_value
